14ef60efa3634739467ab98c8f68065353152d79
michaelkuznicki <mkuznicki@franciscan.edu>
2024-02-20 09:22:06 -0500

    Wrote m1-m3

---
+++ src/m2-for_loops.py
@@ -0,0 +1,44 @@
+###############################################################################
+# TODO: 1. (2 pts)
+#   
+#   For this _TODO_, first, copy the line of code from m1 where you created your list and paste it under this _TODO_. We will use this list some more here.
+#
+#   Now, write a for loop that will loop through each item in the list and print the item.
+#
+#   When you run your code, you should notice that each item prints on its own line.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 2. (3 pt)
+#   
+#   Let's try using an if statement inside a for loop.
+#   
+#   For this _TODO_, write a for loop that loops through your list from todo #1. DO NOT copy and paste your list here, just call it using its variable name.
+#
+#   Inside your loop, include an if statement that checks if "sugar" is in your list of ingredients. If it finds "sugar" in the list, it should print this:
+#
+#   "I found sugar!"
+#
+#   Feel free to modify your list to either include sugar or not include sugar to make sure your loop works correctly.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 3. (3 pts)
+#   
+#   For this _TODO_, write a for loop that uses the range() function to print out lines of stars (*) in increasing amounts ending with a line of 5 stars. So it should print the empty string on the first line, then * on the next line, then ** on the next line, and so on. Your final output should look like this:
+#
+#
+#   *
+#   **
+#   ***
+#   ****
+#   *****
+#
+#   NOTE: Remember, lists start with index 0.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
---
+++ src/m3-functions_with_loops.py
@@ -0,0 +1,54 @@
+###############################################################################
+# TODO: 1. (4 pts)
+#   
+#   Now, let's try to combine a few of the things we learned in previous exercises together. We are going to improve upon some of the loops that we created in m2. Feel free to copy and paste them as you see fit.
+#
+#   For this _TODO_, write a function called ingredient_search() that takes two parameters:
+#       - ingredient    <- str
+#       - list          <- list of strs
+#
+#   It should loop through the given list of ingredients and check if the given ingredient is in the list. If it is, the function should return the value True. If it is not, it should return the value False.
+#
+#   Notice that you can't simply put an else statement with your if statement because the function will only look at the first item in the list. There are a couple of ways you can solve this, but take your best shot at it ask me if you have any questions.
+#
+#   Once you have done this, write 2 lines of code:
+#       1. one where your function will return True
+#       2. and another where your function will return False
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 1. (3 pts)
+#   
+#   For this _TODO_, write a function called triangle() that takes one parameter:
+#       - size  <- int
+#
+#   This function should print a triange of stars (just like m2 todo #3) where the last line (the largest line) has the same number of stars as the size parameter indicates.
+#
+#   So, if the function is called like so:
+#
+#       triangle(2)
+#
+#   it should print
+#
+#
+#       *
+#       **
+#
+#   OR if it is called like this:
+#
+#       triangle(4)
+#
+#   it should print
+#
+#
+#       *
+#       **
+#       ***
+#       ****
+#
+#   Once you have done, this write a line of code that calls the function and prints a triangle of stars with the size 10.
+#   
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
